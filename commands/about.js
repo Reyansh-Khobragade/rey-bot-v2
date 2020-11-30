ed0507e054cb3e33416fc46640f73ca0120e3a32
Reyansh-Khobragade <74308448+Reyansh-Khobragade@users.noreply.github.com>
2020-11-30 16:24:44 +0530

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,9 +1,10 @@
+const config = require('../config')
 const { MessageEmbed } = require("discord.js");
 
 exports.run = async(client, message, args) => {
   let embed = new MessageEmbed()
   .addField('About Me', 'A Simple Discord Bot Which Is Fun To Use')
-  .addField('.help','to know more about the bot use commands `.help`')
+  .addField('.help','to know more about the bot use commands ' + `${config.prefix}` + 'help')
   .addField('Version', '2.2')
       .setColor("RANDOM")
       .setTimestamp()
